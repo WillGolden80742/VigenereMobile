0e5ce6241ae314d92fb3ffbedc336d4d05696d45
WillGolden80742 <william80742@gmail.com>
2022-09-17 19:46:33 -0300

    aumentando

--- app/src/main/java/com/example/vigenere_mobile/MainActivity.java
+++ app/src/main/java/com/example/vigenere_mobile/MainActivity.java
@@ -1,21 +1,19 @@
 package com.example.vigenere_mobile;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
 import android.content.ClipboardManager;
 import android.view.View;
 import android.widget.Button;
 import android.content.ClipData;
-import android.content.Context;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import java.util.Base64;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText output, input, chaveField;
     Button cifrar, decifrar, copyInput, copyOutput, clearInput, clearOutPut;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,10 +79,28 @@ public class MainActivity extends AppCompatActivity {
         clearOutPut = findViewById(R.id.clearOutput);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String encode64(String input) {
+        return Base64.getEncoder().encodeToString(input.getBytes());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String decode64(String input) {
+        byte[] decodedBytes = Base64.getDecoder().decode("");
+        try {
+            decodedBytes = Base64.getDecoder().decode(input);
+        } catch (IllegalArgumentException ex) {
+            output.setText("Decodificação falha");
+        }
+        return new String(decodedBytes);
+    }
+
     private final Runnable cifrarV = new Runnable() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void run() {
             String texto = input.getText().toString();
+            String textoCifrado = "";
             String chave = chaveField.getText().toString();
             int ascii = 0, contKey = 0, sizeKey = chave.length() - 1, sizeChar = texto.length();
             output.setText("");
@@ -93,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                     ascii = (int) texto.charAt(i);
                     ascii += (int) chave.charAt(contKey);
                     try {
-                        output.setText(output.getText().toString() + ((char) ascii));
+                        textoCifrado+=output.getText().toString() + ((char) ascii);
                     } catch (Exception ex) {
 
                     }
@@ -103,15 +119,18 @@ public class MainActivity extends AppCompatActivity {
                         contKey = 0;
                     }
                 }
+                output.setText(encode64(textoCifrado));
             }
         }
     };
 
     private final Runnable decifrarV = new Runnable() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void run() {
-            String texto = input.getText().toString();
+            String texto = decode64(input.getText().toString());
             String chave = chaveField.getText().toString();
+            String textoDecifrado = "";
             int ascii = 0, contKey = 0, sizeKey = chave.length() - 1, sizeChar = texto.length();
             output.setText("");
             if (!chave.equals("")) {
@@ -119,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
                     ascii = (int) texto.charAt(i);
                     ascii -= (int) chave.charAt(contKey);
                     try {
-                        output.setText(output.getText().toString() + ((char) ascii));
+                        textoDecifrado+=output.getText().toString() + ((char) ascii);
                     } catch (Exception ex) {
 
                     }
@@ -129,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
                         contKey = 0;
                     }
                 }
+                output.setText(textoDecifrado);
             }
         }
     };
